bd3710210b49760a697af3524e59bc17f2b5e2ee
Nigel Johnston <nigel.johnston@newvoicemedia.com>
2017-08-08 08:48:35 +0100

    buildkite stuff

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-
-get_previous_commit() {
-	previous_build="$(($BUILDKITE_BUILD_NUMBER - 1))"
-
-	response=`curl "https://api.buildkite.com/v2/organizations/newvoicemedia/pipelines/monotest/builds/${previous_build}?access_token=477f1c395119a00cf113653a41f6e09d1cdda979"`
-
-	previous_commit=`echo $response | jq '.commit'`
-
-	return previous_commit
-}
-
 create_pipeline() {
-	proj=$1
+	local proj=$1
 	# begin the pipeline.yml file
 	echo "steps:"
 	echo "  - trigger: $proj"
@@ -32,23 +21,27 @@ BUILDKITE_BUILD_NUMBER="10"
 # exit immediately on failure, or if an undefined variable is used
 set -eu
 
-# find the directory that changed
+# get the previous commit, so we can do a diff
 
 previous_build="$(($BUILDKITE_BUILD_NUMBER - 1))"
 
 response=`curl "https://api.buildkite.com/v2/organizations/newvoicemedia/pipelines/monotest/builds/${previous_build}?access_token=477f1c395119a00cf113653a41f6e09d1cdda979"`
 
-#echo "$response"
-
 previous_commit=`echo $response | jq '.commit'`
 
-echo $previous_commit
+# strip quotes
 previous_commit=`sed -e 's/^"//' -e 's/"$//' <<<"$previous_commit"`
 
-#diffs=`git diff --name-only 6344963338f16576ea885589f7792637f1f4dde7 732054c8317b27e7b8e4e31cb23159fbaf5fdfd8 `
+# diff to get the changed files
+#diffs=`git diff --name-only ${previous_commit} ${BUILDKITE_COMMIT} `
+#echo "$diffs"
 
-diffs=`git diff --name-only ${previous_commit} ${BUILDKITE_COMMIT} `
-echo "$diffs"
-
+array=`git diff --name-only ${previous_commit} ${BUILDKITE_COMMIT} | sort -u | awk 'BEGIN {FS="/"} {print $1}' | uniq`
+#printf -- "%s\n" "${array[@]}"
+#printf "\n"
+for element in $array
+do
+    echo $element
+done
 create_pipeline "Foo"
 
